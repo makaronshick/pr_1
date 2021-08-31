b732d5b53d8e378afb222d16f07f4f081c07ed95
Andrii <makaronshick@ukr.net>
2021-08-31 21:30:31 +0300

    Added array_flip.php file

---
+++ array_flip.php
@@ -0,0 +1,25 @@
+<?php
+
+function my_array_flip(array $array): array {
+    $newArray = [];
+
+    foreach($array as $key => $value){
+        if (is_int($value) || is_string($value)){
+            $newArray[$value] = $key;
+        } else {
+            echo "Warning:  my_array_flip(): Can only flip STRING and INTEGER values!" . '<br>';
+        }
+    }
+    echo '<br>';
+    return $newArray;
+}
+
+
+
+$input = array("1oranges", "apples", "pears", 12.5, 3, 3, 3);
+
+echo '<pre>';
+var_dump(my_array_flip($input));
+echo '<pre>';
+echo '<br>';
+var_dump(array_flip($input));      // проверка результатов рабочей функцией
